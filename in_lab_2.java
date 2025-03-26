923a14c109249a1b91e8898603f78bb31c2586c1
PamothKumarasinghe <nikolailaazz2@gmail.com>
2025-03-26 08:52:42 +0530

    Update in_lab_2.java

--- in_lab_2.java
+++ in_lab_2.java
@@ -1,3 +1,24 @@
+class InvalidMovieCodeException extends Exception {
+    public InvalidMovieCodeException(String message) {
+        super(message);
+    }
+}
+class InvalidDateOrShowtimeException extends Exception {
+    public InvalidDateOrShowtimeException(String message) {
+        super(message);
+    }
+}
+class InvalidTicketQuantityException extends Exception {
+    public InvalidTicketQuantityException(String message) {
+        super(message);
+    }
+}
+class OverbookingException extends Exception {
+    public OverbookingException(String message) {
+        super(message);
+    }
+}
+
 public class in_lab_2 {
     public static void main() {
         
